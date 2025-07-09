e42ea30da07c4b08b88534c531ad8fc8408839c8
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2025-07-09 22:44:02 +0300

    add tzdata

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule NorthwindElixirTraders.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto_sqlite3, "~> 0.18.1"}
+      {:ecto_sqlite3, "~> 0.18.1"},
+      {:tzdata, "~> 1.1"}
     ]
   end
 end
