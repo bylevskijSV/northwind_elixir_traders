f75517dd6c0df62310f2eb13d4c87dedef3322a3
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2025-07-09 22:44:42 +0300

    refactor Category model

--- priv/repo/migrations/20250706085452_create_categories.exs
+++ priv/repo/migrations/20250706085452_create_categories.exs
@@ -2,12 +2,11 @@ defmodule NorthwindElixirTraders.Repo.Migrations.CreateCategories do
   use Ecto.Migration
 
   def change do
-    create table(:categories, primary_key: false) do
-      add :category_id, :integer, primary_key: true
-      add :name, :string
+    create table(:categories) do
+      add :name, :string, null: false
       add :description, :string
-    end
 
-    create unique_index(:categories, [:category_id])
+      timestamps(type: :utc_datetime)
+    end
   end
 end
