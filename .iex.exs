18d8953cfabab43ea6aa6783232250b5b7b67023
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2025-07-29 22:05:56 +0300

    add order detail

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
-alias NorthwindElixirTraders.{Repo, Supplier, Employee, Category, Product, DataImporter, Shipper, PhoneNumbers, Country, Customer}
+alias NorthwindElixirTraders.{Repo, Supplier, Employee, Category, Product, DataImporter, Shipper, PhoneNumbers, Country, Customer, OrderDetail}
 IEx.configure(inspect: [charlists: :as_lists])
 
  url = "https://raw.githubusercontent.com/datasets/country-codes/2ed03b6993e817845c504ce9626d519376c8acaa/data/country-codes.csv"
