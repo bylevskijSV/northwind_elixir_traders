914d52d5a4c27c32d2c0d353248c8e6b0adfd956
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2025-08-09 23:56:19 +0300

    use Query in eix session

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
 alias NorthwindElixirTraders.{Repo, Supplier, Employee, Category, Product, DataImporter, Shipper, PhoneNumbers, Country, Customer, Order, OrderDetail}
 IEx.configure(inspect: [charlists: :as_lists])
 
- url = "https://raw.githubusercontent.com/datasets/country-codes/2ed03b6993e817845c504ce9626d519376c8acaa/data/country-codes.csv"
+import Ecto.Query
